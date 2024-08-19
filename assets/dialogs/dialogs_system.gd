ced9c52ad79c4f9d5fb7e6e0311f1c06466cd3d4
RobolabGs2 <slavikelk@yandex.ru>
2024-08-19 16:52:21 +0300

    Init dialog system

---
+++ assets/dialogs/dialogs_system.gd
@@ -0,0 +1,12 @@
+extends MarginContainer
+
+@onready var subtitles: RichTextLabel = $Subtitles
+
+var speechers: Dictionary
+
+func register(speecher: Speecher):
+	speechers[speecher.author] = speecher
+
+func play(line: SpeechLine):
+	subtitles.text = "[center]%s\n%s[/center]" % [line.author, line.line]
+	await speechers[line.author].speech(line)
--- assets/dialogs/speech_line.gd
+++ assets/dialogs/speech_line.gd
@@ -3,5 +3,5 @@ extends Resource
 class_name SpeechLine
 
 @export var author: StringName
-@export var line: String
+@export_multiline var line: String
 @export var sound: AudioStream
